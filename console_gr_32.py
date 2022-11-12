f3f90e518f5a169a0084d7667a0f0515c820da9a
adelbrx <ahmed-adel.bereksi@student.unamur.be>
2022-11-12 17:10:37 +0100

    the final version of the mini project of microbit

--- console_gr_32.py
+++ console_gr_32.py
@@ -4,27 +4,27 @@ import random
 import radio
 import microbit
 
-
-
 # definition of functions
+
+
 def get_message():
     """Wait and return a message from another micro:bit.
-    
+
     Returns
     -------
     message: message sent by another micro:bit (str)    
     """
-    message = None    
+    message = None
     while message == None:
         microbit.sleep(250)
         message = radio.receive()
-    
+
     return message
 
 
-def create_new_piece(pieces,board):
-    """function create a random piece and return this piece and the state of board and if it will be a collision
-    
+def create_new_piece(pieces, board):
+    """function create a random piece and return this piece and the state of board
+
     Parameters
     ----------
     pieces : all pieces we can create (dict)
@@ -33,29 +33,49 @@ def create_new_piece(pieces,board):
     Returns
     -------
     random_piece : the new piece created (list)
-    is_collide : True if it will be a collision after the creation of the new piece (bool)
     board : state of the board (list)
+
     """
-    
-    is_collide = False
 
-    random_number = random.randint(8)
-    random_piece = pieces[random_number]
+    random_piece = pieces[random.randint(0, 2)]
 
-    for point in random_piece : 
+    if not is_game_over(random_piece, board):
 
-        if board[point[1]][point[0]] == 9:
-            is_collide = True
-        else :    
-            board[point[1]][point[0]] = 5
+        for case in random_piece:
+            board[case[1]][case[0]] = 5
 
-       
+        return random_piece, board
+    else:
 
-    return random_piece , is_collide , board  
+        return random_piece, board
+
+
+def is_game_over(piece, board):
+    """function test if it will be a collision after the creation of new piece so it tests 
+    if the game is over or not
+
+    Parameters
+    ----------
+    piece : the piece created (dict)
+    board : state of the board (list)
+
+    Return : 
+    --------
+    is_over : True if it will be a collision after the creation of the new piece (bool)
+    """
+
+    is_over = False
+
+    for case in piece:
+        if board[case[1]][case[0]] == 9:
+            is_over = True
+
+    return is_over
+
 
 def crypt_board(board):
     """function transforms the state of the board list of lists to a string
-    
+
     Parameters
     ----------
     board : the state of the board (list)
@@ -64,154 +84,205 @@ def crypt_board(board):
     -------
     result : state of the board after the cryption (str)
     """
+
     result = ""
-    for line in board :
-        for element in line : 
-            result += str(element)
-        result +=","
+
+    for line in board:
+        for column in line:
+            result += str(column)
+        result += ","
+
     return result[0:len(result)-1]
 
-def drop_piece(piece,board):
-    """function change state piece cases on the board state
-    
+
+def is_collide(direction, piece, board):
+    """function test if it will be a collision after the movement or not
+
     Parameters
     ----------
-    piece : the piece created (list)
-    board : the state of the board (list)
+    direction : the direction of the movement (str)
+    piece : the piece created (dict)
+    board : state of the board (list)
+
+    Return : 
+    --------
+    is_piece_collide : True if it will be a collision after the movement of the new piece (bool)
+    """
+
+    is_piece_collide = False
+
+    if direction == "left":
+
+        for case in piece:
+
+            if (case[0] == 0) or ((board[case[1]][case[0] - 1] == 9) and not([case[0]-1, case[1]] in piece)):
+                is_piece_collide = True
+
+    elif direction == "right":
+
+        for case in piece:
+
+            if (case[0] == 4) or ((board[case[1]][case[0] + 1] == 9) and not([case[0]+1, case[1]] in piece)):
+                is_piece_collide = True
+
+    elif direction == "top":
+
+        for case in piece:
+
+            if (case[1] == 0) or ((board[case[1] - 1][case[0]] == 9) and not([case[0], case[1]-1] in piece)):
+                is_piece_collide = True
+
+    elif direction == "down":
+
+        for case in piece:
+
+            if (case[1] == 4) or ((board[case[1] + 1][case[0]] == 9) and not([case[0], case[1]+1] in piece)):
+                is_piece_collide = True
+
+    return is_piece_collide
+
+
+def move_left(piece, board):
+    """Move the piece to left 
+
+    Parameters
+    ----------
+    piece : the piece created (dict)
+    board : state of the board (list)
 
     Returns
     -------
-    board : the state of board (list)
+    modified_piece : piece after the movement (list)
+    board : state of the board (list)   
     """
 
-    for case in piece :
-        board[ case[1] ][ case[0] ] = 9
+    modified_piece = list()
+
+    if not is_collide("left", piece, board):
+
+        for case in piece:
+            board[case[1]][case[0]] = 0
+
+        for case in piece:
+            board[case[1]][case[0]-1] = 5
+            modified_piece.append([case[0]-1, case[1]])
+
+        return modified_piece, board
+    else:
+        modified_piece = piece
+        return modified_piece, board
+
+
+def move_right(piece, board):
+    """Move the piece to right 
+
+    Parameters
+    ----------
+    piece : the piece created (dict)
+    board : state of the board (list)
+
+    Returns
+    -------
+    modified_piece : piece after the movement (list)
+    board : state of the board (list)
+    """
+
+    modified_piece = list()
+
+    if not is_collide("right", piece, board):
+
+        for case in piece:
+            board[case[1]][case[0]] = 0
+
+        for case in piece:
+            board[case[1]][case[0]+1] = 5
+            modified_piece.append([case[0]+1, case[1]])
+
+        return modified_piece, board
+    else:
+        modified_piece = piece
+        return modified_piece, board
+
+
+def move_top(piece, board):
+    """Move the piece to top 
+
+    Parameters
+    ----------
+    piece : the piece created (dict)
+    board : state of the board (list)
+
+    Returns
+    -------
+    modified_piece : piece after the movement (list)
+    board : state of the board (list)
+    """
+
+    modified_piece = list()
+
+    if not is_collide("top", piece, board):
+
+        for case in piece:
+            board[case[1]][case[0]] = 0
+
+        for case in piece:
+            board[case[1]-1][case[0]] = 5
+            modified_piece.append([case[0], case[1]-1])
+
+        return modified_piece, board
+    else:
+        modified_piece = piece
+        return modified_piece, board
+
+
+def move_down(piece, board):
+    """Move the piece to down 
+
+    Parameters
+    ----------
+    piece : the piece created (dict)
+    board : state of the board (list)
+
+    Returns
+    -------
+    modified_piece : piece after the movement (list)
+    board : state of the board (list)
+    """
+
+    modified_piece = list()
+
+    if not is_collide("down", piece, board):
+
+        for case in piece:
+            board[case[1]][case[0]] = 0
+
+        for case in piece:
+            board[case[1]+1][case[0]] = 5
+            modified_piece.append([case[0], case[1]+1])
+
+        return modified_piece, board
+    else:
+        modified_piece = piece
+        return modified_piece, board
+
+
+def drop_piece(piece, board):
+    """droping the piece in the board
+
+    Parameters
+    ----------
+    piece : the piece created (dict)
+    board : state of the board (list)
+
+    Returns
+    -------
+    board : state of the board (list)
+    """
+
+    for case in piece:
+        board[case[1]][case[0]] = 9
+
     return board
-
-def left_move_piece(piece,board):
-    """function moves piece cases to left on the board state 
-    
-    Parameters
-    ----------
-    piece : the piece created (list)
-    board : the state of the board (list)
-
-    Returns
-    -------
-    list_final : list after moving (list)
-    board : the state of board (list)
-    """
-    collision = False
-    for case in piece:
-        if (case[0] == 0) or (not([case[0]-1 , case[1]] in piece) and board[ case[1] ][ case[0] - 1 ] == 1):
-            collision = True
-
-    if not collision : 
-
-        list_final = list()
-
-        for case in piece :
-            board[case[1]][case[0]] = 0
-            list_final.append([case[0]-1 , case[1]])
-            board[case[1]][case[0] - 1] = 5
-
-        return list_final , board   
-    else :
-        return piece , board        
-
-def right_move_piece(piece,board):
-    """function moves piece cases to right on the board state 
-    
-    Parameters
-    ----------
-    piece : the piece created (list)
-    board : the state of the board (list)
-
-    Returns
-    -------
-    list_final : list after moving (list)
-    board : the state of board (list)
-    """
-    collision = False
-    for case in piece:
-        if (case[0] == 4) or (not([case[0] + 1,case[1]] in piece) and board[ case[1] ][ case[0] + 1 ] == 1):
-            collision = True
-    if not collision : 
-        list_final = list()
-
-        for case in piece :
-            board[case[1]][case[0]] = 0
-            list_final.append([case[0]+1 , case[1]])
-            board[case[1]][case[0] + 1] = 5
-
-        return list_final , board  
-    else :
-        return piece , board        
-
-def top_move_piece(piece,board):
-    """function moves piece cases to top on the board state 
-    
-    Parameters
-    ----------
-    piece : the piece created (list)
-    board : the state of the board (list)
-
-    Returns
-    -------
-    list_final : list after moving (list)
-    board : the state of board (list)
-    """
-    collision = False
-
-    for case in piece:
-        if (case[1] == 0) or (not([case[0],case[1]-1] in piece) and board[ case[1] - 1][ case[0] ] == 1):
-            collision = True
-
-    if not collision : 
-
-        list_final = list()
-        for case in piece :
-            board[case[1]][case[0]] = 0
-            list_final.append([case[0] , case[1]-1])
-            board[case[1] - 1][case[0]] = 5
-
-        return list_final , board   
-    else :
-        return piece , board        
-
-def down_move_piece(piece,board):
-    """function moves piece cases to down on the board state 
-    
-    Parameters
-    ----------
-    piece : the piece created (list)
-    board : the state of the board (list)
-
-    Returns
-    -------
-    list_final : list after moving (list)
-    board : the state of board (list)
-    """
-    collision = False
-
-    for case in piece:
-        if (case[1] == 4) or (not([case[0],case[1]+1] in piece) and board[ case[1] + 1][ case[0] ] == 1):
-            collision = True
-
-    if not collision : 
-
-        after_move = list()
-        for case in piece :
-            board[case[1]][case[0]] = 0
-            after_move.append([case[0] , case[1]+1])
-            board[case[1] + 1][case[0] ] = 5
-
-        return after_move , board 
-    else :
-        return piece , board        
-            
-                  
-            
 
 
 # settings
@@ -223,25 +294,17 @@ radio.config(group=group_id)
 
 # create empty board + available pieces
 board = [
-    [0,0,0,0,0],
-    [0,0,0,0,0],
-    [0,0,0,0,0],
-    [0,0,0,0,0],
-    [0,0,0,0,0]
-    ]
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+]
+piece_0 = [[0, 0]]
+piece_1 = [[0, 0], [1, 0]]
+piece_2 = [[0, 0], [0, 1]]
 
-microbit.display.clear()
-
-piece_0 = [[0,0] , [0,1] , [1,0] , [1,1]]
-piece_1 = [[0,0] , [0,1] , [1,0]]
-piece_2 = [[0,0] , [1,0] , [1,1]]
-piece_3 = [[0,0] , [0,1] , [1,1]]
-piece_4 = [[0,1] , [1,0] , [1,1]]
-piece_5 = [[0,0] , [0,1]]
-piece_6 = [[0,0] , [1,0]]
-piece_7 = [[0,0]]
-
-pieces = {0:piece_0 , 1:piece_1 , 2:piece_2 , 3:piece_3 , 4:piece_4 , 5:piece_5 , 6:piece_6 , 7:piece_7}
+pieces = {0: piece_0, 1: piece_1, 2: piece_2}
 
 # loop until game is over
 nb_dropped_pieces = 0
@@ -252,38 +315,42 @@ while not game_is_over:
     microbit.display.show(nb_dropped_pieces)
 
     # create a new piece in the top left corner
-    new_piece , is_collide , board = create_new_piece(pieces,board)
-    
+    new_piece, board = create_new_piece(pieces, board)
+
     # check if the new piece collides with dropped pieces
-    game_is_over = is_collide
-    
+    game_is_over = is_game_over(new_piece, board)
+
     if not game_is_over:
         # ask orders until the current piece is dropped
         piece_dropped = False
         while not piece_dropped:
             # send state of the board to gamepad (as a string)
             radio.send(crypt_board(board))
-            
+
             # wait until gamepad sends an order
             order = get_message()
-            
+
             # execute order (drop or move piece)
-            if order == "drop" : 
-                board = drop_piece(new_piece,board)
-            elif order == "left" :
-                new_piece ,board = left_move_piece(new_piece,board)
-            elif order == "right" :
-                new_piece ,board = right_move_piece(new_piece,board)
-            elif order == "top" :
-                new_piece ,board = top_move_piece(new_piece,board)
-            elif order == "down" :
-                new_piece ,board = down_move_piece(new_piece,board)
-            else :
+            if order == "left":
+                new_piece, board = move_left(new_piece, board)
+            elif order == "right":
+                new_piece, board = move_right(new_piece, board)
+            elif order == "top":
+                new_piece, board = move_top(new_piece, board)
+            elif order == "down":
+                new_piece, board = move_down(new_piece, board)
+            elif order == "drop":
+                board = drop_piece(new_piece, board)
+                piece_dropped = True
+                nb_dropped_pieces += 1
+                new_piece = list()
+            else:
                 pass
-        
+
         # wait a few milliseconds and clear screen
         microbit.sleep(500)
         microbit.display.clear()
-    
+
 # tell that the game is over
 microbit.display.scroll('Game is over', delay=100)
+microbit.display.show(nb_dropped_pieces)
--- gamepad_gr_32.py
+++ gamepad_gr_32.py
@@ -2,84 +2,93 @@ import radio
 import microbit
 
 # definition of functions
+
+
 def get_message():
     """Wait and return a message from another micro:bit.
-    
+
     Returns
     -------
-    message: message sent by another micro:bit (str)
-        
+    message: message sent by another micro:bit (str)    
     """
-    
-    message = None    
+    message = None
     while message == None:
         microbit.sleep(250)
         message = radio.receive()
-    
+
     return message
-    
-def decrypt_board(state):
+
+
+def decrypt_board(view):
     """function transform state of table from string to list of lists
-    
+
     parameters
     ----------
-    state : the state of the table (str)
+    view : the state of the board (str)
 
     Returns 
     -------
     board : the state of board in the final type list (list)
     """
-    board = list()
-    el = list()
-    for character in state : 
-        if character != "," : 
-            el.append(int(character))
-        elif character == "," :
-            board.append(el)
-            el = list()
-    board.append(el)
 
-    return board 
+    board = list()
+    line = list()
+
+    for character in view:
+        if character != ",":
+            line.append(int(character))
+        elif character == ",":
+            board.append(line)
+            line = list()
+
+    board.append(line)
+
+    return board
+
 
 def show_board(board):
-    """function alight cases of the microbit wich have the same coordinate with the state of table
-    
+    """show the content of the board in the microbit 
+
     Parameters
     ----------
-    board : state of the board (list)
+    board : the state of the board (str)
+
     """
+
     i = 0
-    for line in board :
+    for line in board:
         j = 0
-        for column in line :
-            if column > 0 :
-                microbit.display.set_pixel(j,i,column)
+        for column in line:
+            microbit.display.set_pixel(j, i, column)
             j += 1
-        i += 1   
-            
+        i += 1
+
+
 def get_direction():
-    """function return the direction of the acceleremoter in the microbit 
-    
-    Returns 
+    """Return the direction after the movement of the micobit
+
+    Returns
     -------
-    direction : the direction of the mouvement in the microbit (str)
+    direction : the direction of the microbit (str)
     """
+
     direction = "nothing"
     x = microbit.accelerometer.get_x()
     y = microbit.accelerometer.get_y()
-        
-    if x > 20 :
+
+    if x > 200:
         direction = "right"
-    elif x < -20 : 
+    elif x < -200:
         direction = "left"
-    elif y > 20 :
-        direction = "top"
-    elif y < -20 :
+    elif y > 200:
         direction = "down"
-    else :
-        direction = "nothing"                
+    elif y < -200:
+        direction = "top"
+    else:
+        direction = "nothing"
 
     return direction
+
 
 # settings
 group_id = 32
@@ -87,26 +96,24 @@ group_id = 32
 # setup radio to receive/send messages
 radio.on()
 radio.config(group=group_id)
-    
+
 # loop forever (until micro:bit is switched off)
 while True:
     # get view of the board
     view = get_message()
-    
+
     # clear screen
     microbit.display.clear()
-    
+
     # show view of the board
-    board = decrypt_board(view)
-    show_board(board)
-    
-    
+    show_board(decrypt_board(view))
+
     # wait for button A or B to be pressed
     while not (microbit.button_a.is_pressed() or microbit.button_b.is_pressed()):
         microbit.sleep(50)
 
     if microbit.button_a.is_pressed():
-        # send current direction       
+        # send current direction
         radio.send(get_direction())
     elif microbit.button_b.is_pressed():
         # notify that the piece should be dropped
